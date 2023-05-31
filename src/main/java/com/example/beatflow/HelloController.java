746c9811f662f6c4c0be578b3495cc6dfa7f1cab
AmyGillioz <amy.gillioz@hes-so.ch>
2023-05-31 13:04:47 +0200

    Controllor_User

--- src/main/java/com/example/beatflow/HelloController.java
+++ src/main/java/com/example/beatflow/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.beatflow;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -9,6 +10,7 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("User");
+
     }
 }
