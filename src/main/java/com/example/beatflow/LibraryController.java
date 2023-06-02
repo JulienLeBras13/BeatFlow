8e84f7cdbf15ac2482575ef72d61c2cbd0b3dd41
TheWall8 <francois.clement2002@gmail.com>
2023-06-02 10:53:21 +0200

    Adding view and controller for Library part, PART2

--- src/main/java/com/example/beatflow/LibraryController.java
+++ src/main/java/com/example/beatflow/LibraryController.java
@@ -1,14 +1,31 @@
 package com.example.beatflow;
 
+// Using CSV files
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+// Using graphical elements
 import javafx.fxml.FXML;
-import java.util.ArrayList;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 
 public class LibraryController {
+    // Graphical objects used
+    @FXML
+    private TextField txtTitle;
+    @FXML
+    private TextField txtKind;
+    @FXML
+    private TextField txtArtist;
+    @FXML
+    private TextField txtAlbum;
+
+
     // List of music types
     String typesOfMusic [] = {"Rap", "Classical", "Electro"};
     // Conversion to arrayList
-    ArrayList<String> typesList = new ArrayList<>();
+    //ArrayList<String> typesList = new ArrayList<>();
 
     // Tab of playlists declaration (where "Classical, Rap, ..." will be written)
     @FXML
@@ -19,4 +36,19 @@ public class LibraryController {
     {
 
     }*/
+
+    /*
+    * Function name : btnAdd_Click
+    * Creation date : 31.05.2023
+    * Function goal : Adding a music on the CSV file with the user written values
+    * */
+    @FXML
+    protected void btnAdd_Click()
+    {
+        // Verification that title and .mp3 fields aren't empty
+        if(txtTitle.getText() != "")
+        {
+
+        }
+    }
 }
