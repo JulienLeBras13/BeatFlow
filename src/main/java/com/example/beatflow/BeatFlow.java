b4eea33703c2f05fb1d745e2b47989561b5904e2
JulienLeBras13 <julien.lebras@hes-so.ch>
2023-06-02 11:47:35 +0200

    Test new App

--- src/main/java/com/example/beatflow/BeatFlow.java
+++ src/main/java/com/example/beatflow/BeatFlow.java
@@ -8,17 +8,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class BeatFlow extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BeatFlowDel.class.getResource("InitView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(BeatFlow.class.getResource("InitView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1300, 900);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        launch();
     }
 }
