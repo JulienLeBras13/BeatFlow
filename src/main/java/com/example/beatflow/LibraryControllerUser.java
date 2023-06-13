af9e331a32a58610db7c2f32740cb8c883ca2e58
AmyGillioz <amy.gillioz@hes-so.ch>
2023-06-13 14:34:57 +0200

    LibraryControllerUser methode showLibreryTitles et showData completed

--- src/main/java/com/example/beatflow/LibraryControllerUser.java
+++ src/main/java/com/example/beatflow/LibraryControllerUser.java
@@ -12,12 +12,13 @@ import models.Artist;
 import models.Library;
 import models.Song;
 
+import java.nio.file.WatchEvent;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class LibraryControllerUser {
     @FXML
-    private Label logo, dataTitle,dataNameArtist, search;
+    private Label logo, dataTitle,dataNameArtist, dataKind , search;
     @FXML
     private Button buttonSearch, buttonAddToPlaylist, library, buttonNewPlaylist;
     @FXML
@@ -27,6 +28,7 @@ public class LibraryControllerUser {
     private ArrayList<Song> selectedPlayList = new  ArrayList();
     @FXML
     private ArrayList<Artist> selectedMusic = new ArrayList() ;
+
     @FXML
     private Pane paneInformationMusic;
     @FXML
@@ -46,7 +48,6 @@ public class LibraryControllerUser {
             listViewTitle.getItems().add(song.getTitle());
         }
     }
-
     @FXML
     protected void showData(){
         int index = listViewTitle.getSelectionModel().getSelectedIndex();
@@ -54,21 +55,18 @@ public class LibraryControllerUser {
         Song song = selectedPlayList.get(index);
         dataTitle.setText(song.getTitle());
         //Show data artis
-        Artist artist = selectedMusic.get(index);
-        dataNameArtist.setText(artist.getArtistName());
-    }
-    /*public ListView<String> getListViewPlaylist(){
-        return this.Playlists;
+        dataNameArtist.setText(song.getArtist().getArtistName());
+        //Show data kind
+        dataKind.setText(song.getKind());
+
     }
     @FXML
     protected void search(){
         // compare value inputUser to value in Playlist
-    for(String item : Playlists.getItems()){
-        if(item.toLowerCase().contains((CharSequence) textEnter)){
-            listViewTitle.getItems().add(item);
-        }
+        // Display song in listview
+        selectedPlayList.clear();
+        Song findedSong = BeatFlow.findSong(selectedPlayList, textEnter.getText());
     }
-    }*/
     @FXML
     protected void CreatNewPlaylist(){
     }
