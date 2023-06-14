9682fe4c2e715c4c674213b7fa200fb48200b939
AmyGillioz <amy.gillioz@hes-so.ch>
2023-06-14 20:49:44 +0200

    LibraryControllerUser completed

--- src/main/java/com/example/beatflow/LibraryControllerUser.java
+++ src/main/java/com/example/beatflow/LibraryControllerUser.java
@@ -17,13 +17,12 @@ public class LibraryControllerUser {
     @FXML
     private TextField textEnter, nomPlaylist;
     @FXML
-    private ListView<String> listViewTitle, playlists, listViewRight;
+    private ListView<String> listViewLeft, playlists, listViewRight;
     private ArrayList<Song> selectedPlayList = new  ArrayList();
     private ArrayList<Song>  chargedPlaylist = new ArrayList<>();
     int indexOfSelectedPlaylist;
     @FXML
     private ArrayList<Artist> selectedMusic = new ArrayList() ;
-
     @FXML
     private Pane paneInformationMusic;
     @FXML
@@ -33,6 +32,7 @@ public class LibraryControllerUser {
     }
     @FXML
     protected void onActualizePlaylist(){
+        // actualize Playlist in listView
         playlists.getItems().clear();
         playlists.getItems().add("library");
         for (Playlist playlist : BeatFlow.library.getPlaylists()){
@@ -45,9 +45,9 @@ public class LibraryControllerUser {
     protected void showTitles() {
         // Display song in listview
         selectedPlayList.clear();
-        listViewTitle.getItems().clear();
+        listViewLeft.getItems().clear();
         String playlistName = playlists.getSelectionModel().getSelectedItem();
-
+        //Display Library as default value
         if (playlistName.equals("library")) {
             selectedPlayList = (ArrayList<Song>) BeatFlow.library.getSongs().clone();
         } else {
@@ -64,7 +64,7 @@ public class LibraryControllerUser {
                 }
             }
         }
-
+        // Display result in listView
         listViewRight.getItems().clear();
         if (!BeatFlow.library.getPlaylists().get(indexOfSelectedPlaylist).getFix()) {
             for (Song song : chargedPlaylist) {
@@ -73,12 +73,12 @@ public class LibraryControllerUser {
         }
         //selection one itm
         for (Song song : selectedPlayList) {
-            listViewTitle.getItems().add(song.getTitle());
+            listViewLeft.getItems().add(song.getTitle());
         }
     }
     @FXML
     protected void showDataLeft(){
-        int index = listViewTitle.getSelectionModel().getSelectedIndex();
+        int index = listViewLeft.getSelectionModel().getSelectedIndex();
         // show data title
         Song song = selectedPlayList.get(index);
         dataTitle.setText(song.getTitle());
@@ -86,9 +86,7 @@ public class LibraryControllerUser {
         dataNameArtist.setText(song.getArtist().getArtistName());
         //Show data kind
         dataKind.setText(song.getKind());
-
     }
-
     @FXML
     protected void showDataRight(){
         int index = listViewRight.getSelectionModel().getSelectedIndex();
@@ -99,14 +97,26 @@ public class LibraryControllerUser {
         dataNameArtist.setText(song.getArtist().getArtistName());
         //Show data kind
         dataKind.setText(song.getKind());
-
     }
     @FXML
     protected void search(){
-        // compare value inputUser to value in Playlist
-        // Display song in listview
-        selectedPlayList.clear();
-        Song findedSong = BeatFlow.findSong(selectedPlayList, textEnter.getText());
+        //clear listviews
+        listViewLeft.getItems().clear();
+        listViewRight.getItems().clear();
+
+        String findedSong = textEnter.getText().toString();
+        // searching song in listview
+        if (BeatFlow.findSong(BeatFlow.library.getSongs(), findedSong) != null) {
+            listViewLeft.getItems().add(listViewLeft.getItems().size(),textEnter.getText());
+        }
+        // code for empty textField
+        else if (textEnter.getText().trim().isEmpty()){
+            selectedPlayList.clear();
+            selectedPlayList = (ArrayList<Song>) BeatFlow.library.getSongs().clone();
+            for (Song song : selectedPlayList){
+                listViewLeft.getItems().add(song.getTitle());
+            }
+        }
     }
     @FXML
     protected void CreatNewPlaylist(){
@@ -114,18 +124,20 @@ public class LibraryControllerUser {
     }
     @FXML
     protected void AddInPlaylist(){
+        // control if playlist is not Fix
         if (!BeatFlow.library.getPlaylists().get(indexOfSelectedPlaylist).getFix()) {
             listViewRight.getItems().clear();
+            // add song in new Playlist
             for (Song song : selectedPlayList) {
-                if (song.getTitle().equals(listViewTitle.getSelectionModel().getSelectedItem())) {
+                if (song.getTitle().equals(listViewLeft.getSelectionModel().getSelectedItem())) {
                     BeatFlow.library.getPlaylists().get(indexOfSelectedPlaylist).getPlaylist().add(song);
                     chargedPlaylist = BeatFlow.library.getPlaylists().get(indexOfSelectedPlaylist).getPlaylist();
                 }
             }
+            // display the song in the listView
             for (Song song : chargedPlaylist) {
                 listViewRight.getItems().add(song.getTitle());
             }
         }
     }
-
 }
