83e8af4093c469f0e778523aaaeda0ffb0b37eed
JulienLeBras13 <julien.lebras@hes-so.ch>
2023-06-14 13:29:44 +0200

    add Btn to show all playlist, adapt method showTitles() to print titles of all song that the selected playlist get.

--- src/main/java/models/Playlist.java
+++ src/main/java/models/Playlist.java
@@ -7,10 +7,15 @@ import java.util.ArrayList;
  */
 public class Playlist {
     private String name;
+    private Boolean fix = false;
     private ArrayList<Song> playlist = new ArrayList<>();
 
     public Playlist(String name){
         this.name = name;
+    }
+    public Playlist(String name, Boolean fix){
+        this.name = name;
+        this.fix = fix;
     }
 
     public ArrayList<Song> getPlaylist(){
@@ -19,5 +24,9 @@ public class Playlist {
 
     public String getName() {
         return name;
+    }
+
+    public Boolean getFix(){
+        return this.fix;
     }
 }
